5b181fe5b4f78f2a56deb6f82cd3336fbae0a2d4
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-10-21 09:15:03 +0200

    21/10/2021
    Continuación del ejercicio 13.

--- codigoPHP/practica13.php
+++ codigoPHP/practica13.php
@@ -28,13 +28,83 @@ Fecha de creación: 19/10/2021
          * Función que cuenta el número de visitas a la página desde una fecha
          */
         
-        $sArchivo = 'prueba.txt';
-        $oArchivoAbierto = fopen($sArchivo, 'x+');
-        fwrite($oArchivoAbierto, 'más');
-        $sRead = fread($oArchivoAbierto, filesize($sArchivo));
-        fclose($oArchivoAbierto);
+        $sFile = '../doc/prueba.txt';
         
-        echo $sRead;
+        /**
+         * file_exists comprueba si el fichero existe.
+         */
+        if(file_exists($sFile)){
+            echo '<ul>';
+            echo '<li>Basename: '. basename($sFile).'</li>';
+            echo '<li>Dirname: '. dirname($sFile).'</li>';
+            echo '<li>Filesize: '. filesize($sFile).'</li>';
+            echo '<li>Tipo de archivo: '. filetype($sFile).'</li>';
+            echo '<li>End of File: '. (feof($sFile)?'final':'no final').'</li>';
+            echo '<li>Último acceso: '. fileatime($sFile).'</li>';
+            echo '<li>Último cambio: '. filectime($sFile).'</li>';
+            echo '<li>Última modificación: '. filemtime($sFile).'</li>';
+            echo '<li>ID Usuario propietario: '. fileowner($sFile).'</li>';
+            echo '<li>ID Grupo del archivo: '. filegroup($sFile).'</li>';
+            echo '<li>Número de inodo: '. fileinode($sFile).'</li>';
+            echo '<li>Permisos del archivo: '. fileperms($sFile).'</li>';
+            echo '</ul>';
+            
+            echo '<h3>Lectura del archivo mediante file()</h3>';
+            /**
+             * Este no necesita que el fichero esté abierto.
+             * Devuelve un array con cada línea del fichero.
+             */
+            $aFileArray = file($sFile);
+            var_dump($aFileArray);
+            print_r($aFileArray);
+            
+            echo '<h4>Texto: </h4>';
+            foreach ($aFileArray as $sLinea) {
+                echo "<p>$sLinea</p>";
+            }
+            
+            echo '<h3>Lectura del archivo mediante file_get_contents()</h3>';
+            /*
+             * Este método no necesita que el fichero esté abierto.
+             * Devuelve un string con el contenido del fichero.
+             */
+            $sFileString = file_get_contents($sFile);
+            var_dump($sFileString);
+            
+            echo '<h4>Texto: </h4>';
+            echo "<p>$sFileString</p>";
+            
+            echo get_include_path();
+            
+        }
+        else{
+            echo 'El fichero no existe';
+        }
+        
+        /*
+         * Filesystem Functions: https://www.w3schools.com/php/php_ref_filesystem.asp
+         * - copy()
+         * - delete()
+         * - fflush()
+         * - fnmatch()
+         * 
+         * - fopen()
+         * - fclose()
+         * - flock()
+         * 
+         * - fgetc()    devuelve caracter de archivo abierto
+         * - fgetcsv()  devuelve linea csv de archivo abierto
+         * - fgets()    devuelve línea de archivo abierto
+         * - fpasstru()
+         * 
+         * - file_put_contents()    fopen + fwrite + fclose
+         * - fputcsv()  formatea una línea como CSV y la escribe en un archivo abierto.
+         * - fputs() == fwrite()
+         * 
+         * ? set_include_path()
+         * 
+         */
+        
         ?>
     </body>
 </html>
